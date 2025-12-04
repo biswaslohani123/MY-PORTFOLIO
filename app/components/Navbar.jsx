c2531b4048b13dbc72cf0936ca1ae4d72de60595
biswasuser <lohanibiswas1@gmail.com>
2025-12-04 20:15:27 +0545

    made navbar

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,10 +1,30 @@
+import { assets } from '@/assets/assets'
+import Image from 'next/image'
 import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>
+    <>
+
+    <nav>
+        <a href="">
+            <Image src={assets.logo} className='w-28 cursor-pointer mt-14'/>
+        </a>
+
+        <ul>
+            <li><a href="#top">Home</a></li>
+            <li><a href="#about">About me</a></li>
+            <li><a href="#services">Services</a></li>
+            <li><a href="#work">My Work</a></li>
+            <li><a href="#contact">Contact me</a></li>
+        </ul>
+
+        <div>
+            <a href="#contact">Contact <Image src={assets.arrow_icon} className='w-3'/></a>
+        </div>
+    </nav>
       
-    </div>
+    </>
   )
 }
 
