ea016ec781077a94b44ea6bcc662541c4a322c89
biswasuser <lohanibiswas1@gmail.com>
2025-12-04 20:58:59 +0545

    comleted navbar section

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,4 +1,13 @@
 const config = {
+  theme: {
+    extend: {
+      fontFamily: {
+        Outfit: ["Outfit", "sans-serif"],
+        Ovo: ["Ovo", "serif"],
+      },
+    },
+  },
+
   plugins: {
     "@tailwindcss/postcss": {},
   },
