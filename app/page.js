fbab69f42958720b998d6f4a6e01a4d0adad896b
biswasuser <lohanibiswas1@gmail.com>
2025-12-05 15:00:24 +0545

    completed About section

--- app/page.js
+++ app/page.js
@@ -3,12 +3,14 @@
 import Image from "next/image";
 import Navbar from "./components/Navbar";
 import Header from "./components/Header";
+import Aboutme from "./components/Aboutme";
 
 export default function Home() {
   return (
     <div>
       <Navbar/>
       <Header/>
+      <Aboutme/>
     </div>
   );
 }
